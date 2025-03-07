2beb7db4807d2468d8879ea56b325a121d723d48
BalaajRaza <balaajraza2135@gmail.com>
2025-03-08 00:24:46 +0500

    Login UI

--- BaatCheet/BaatCheet/Form1.Designer.cs
+++ BaatCheet/BaatCheet/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BaatCheet
 {
-    partial class Form1
+    partial class BaatCheet
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaatCheet));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Tagline = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Poppins Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(380, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 32);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "BaatCheet";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Tagline
+            // 
+            this.Tagline.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tagline.BackColor = System.Drawing.SystemColors.Window;
+            this.Tagline.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Tagline.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Tagline.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tagline.Location = new System.Drawing.Point(380, 130);
+            this.Tagline.Name = "Tagline";
+            this.Tagline.Size = new System.Drawing.Size(140, 20);
+            this.Tagline.TabIndex = 1;
+            this.Tagline.Text = "Dosti Karoge?";
+            this.Tagline.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BaatCheet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.Tagline);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "BaatCheet";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Tagline;
     }
 }
 
--- BaatCheet/BaatCheet/Form1.cs
+++ BaatCheet/BaatCheet/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace BaatCheet
 {
-    public partial class Form1 : Form
+    public partial class BaatCheet : Form
     {
-        public Form1()
+        public BaatCheet()
         {
             InitializeComponent();
+            this.DoubleBuffered = true; // Reduce flickering
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e); // Ensure the base form is painted
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias; // Smooth edges
+
+            // Define the rounded rectangle area
+            int x = (this.ClientSize.Width - 350) / 2; // Center horizontally
+            int y = 180;  // Position below the tagline
+            int width = 350, height = 250, radius = 30;
+
+            GraphicsPath path = CreateRoundedRectangle(x, y, width, height, radius);
+
+            // Fill the rectangle with color
+            Brush fillBrush = new SolidBrush(Color.FromArgb(230, 230, 230)); // Light Gray
+            g.FillPath(fillBrush, path);
+
+            // Draw the border
+            Pen borderPen = new Pen(Color.Gray, 2);
+            g.DrawPath(borderPen, path);
+        }
+
+        private GraphicsPath CreateRoundedRectangle(int x, int y, int width, int height, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddArc(x, y, radius, radius, 180, 90); // Top-left corner
+            path.AddArc(x + width - radius, y, radius, radius, 270, 90); // Top-right
+            path.AddArc(x + width - radius, y + height - radius, radius, radius, 0, 90); // Bottom-right
+            path.AddArc(x, y + height - radius, radius, radius, 90, 90); // Bottom-left
+            path.CloseFigure(); // Connect all points
+
+            return path;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // You can leave this empty or add any desired behavior
         }
     }
 }
