dae1d7f8c51fada996bd961350539d7ad5447cf1
m-wajdan <i230033@isb.nu.edu.pk>
2025-03-08 15:17:26 +0500

    SignUp UI
    Modified the UI

--- BaatCheet/BaatCheet/Form1.Designer.cs
+++ BaatCheet/BaatCheet/Form1.Designer.cs
@@ -29,49 +29,67 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaatCheet));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Tagline = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(621, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BaatCheet";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(656, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dosti Karoge?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label3.Location = new System.Drawing.Point(631, 695);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Already have an account";
             // 
             // textBox1
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox1.Font = new System.Drawing.Font("Poppins Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(380, 100);
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(562, 271);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 32);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "BaatCheet";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // Tagline
-            // 
-            this.Tagline.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Tagline.BackColor = System.Drawing.SystemColors.Window;
-            this.Tagline.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Tagline.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.Tagline.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Tagline.Location = new System.Drawing.Point(380, 130);
-            this.Tagline.Name = "Tagline";
-            this.Tagline.Size = new System.Drawing.Size(140, 20);
-            this.Tagline.TabIndex = 1;
-            this.Tagline.Text = "Dosti Karoge?";
-            this.Tagline.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Size = new System.Drawing.Size(330, 40);
+            this.textBox1.TabIndex = 6;
             // 
             // BaatCheet
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(884, 561);
-            this.Controls.Add(this.Tagline);
+            this.ClientSize = new System.Drawing.Size(1435, 753);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "BaatCheet";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,9 +98,11 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox Tagline;
     }
 }
+
 
